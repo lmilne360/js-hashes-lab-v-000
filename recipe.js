1d13083e7bffa46229c904b691f0a83ebe475f63
Laurent Milne <lmilne360@gmail.com>
2017-01-08 07:57:35 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount) {
+  recipe[ingredient] = amount;
+  return recipe;
+}
+
+function readRecipe(recipe) {
+  for (var key in recipe) {
+    if (object.hasOwnProperty(key)) {
+      console.log(`this recipe calls for ${recipe[key]} of ${key}`);
+    }
+  }
+}
